0806f07b2de6f42c57ae2bd65a3d32cc9cc042a7
Jerguš Lejko <jergus.lejko@gmail.com>
2022-08-10 22:14:17 +0200

    fix path

--- butter/runner.py
+++ butter/runner.py
@@ -3,12 +3,14 @@ from butter import config, supervisor
 from butter.schema import Command
 
 def run(command : Command) -> None:
+    path = config.load().path
     namespace = config.load().name
 
     if command.mode == 'downtime':
         supervisor.stop([f"{namespace}:*"])
 
-    os.chdir(command.working_directory)
+
+    os.chdir(os.path.join(path, command.working_directory))
     os.system(command.command)
     
     if command.mode == 'downtime':
